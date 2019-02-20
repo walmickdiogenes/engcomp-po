51df3fb39686c6709bfcc4d44a2dc317d2634e7b
Walmick Diogenes <walmick.diogenes@gmail.com>
2019-02-20 17:09:15 -0300

    insertion

---
+++ insertion-sort/insertion.py
@@ -0,0 +1,50 @@
+from random import randint
+import matplotlib as mpl
+import timeit
+
+tamlista = [10000, 20000, 30000, 40000, 50000]
+def geraLista(tam):
+    lista = []
+    while len(lista) < tam:
+        n = randint(1,1*tam)
+        if n not in lista: lista.append(n)
+    return lista
+
+def insertionSort(alist):
+   for i in range(1,len(alist)):
+       current = alist[i]
+       while i>0 and alist[i-1]>current:
+            alist[i] = alist[i-1]
+            i = i-1
+            alist[i] = current
+   return alist
+
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+def desenhaGrafico(x,y,ym,yp,xl = "Tamanho", yl = "Tempo"):
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x,y, label = "Melhor Tempo")
+    ax.plot(x,ym, label = "Medio Tempo")
+    ax.plot(x,yp, label = "Pior Tempo")
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    fig.savefig('graph.png')
+
+melhorTempo = []
+piorTempo = []
+medioTempo = []
+
+for i in tamlista:
+  medio = geraLista(i)
+  melhor = sorted(medio)
+  pior = sorted(medio, reverse=True)
+
+  melhorTempo.append(timeit.timeit("insertionSort({})".format(melhor),setup="from __main__ import insertionSort",number=1))
+  piorTempo.append(timeit.timeit("insertionSort({})".format(pior),setup="from __main__ import insertionSort",number=1))
+  medioTempo.append(timeit.timeit("insertionSort({})".format(medio),setup="from __main__ import insertionSort",number=1))
+
+
+
+desenhaGrafico(tamlista,melhorTempo, piorTempo, medioTempo)
