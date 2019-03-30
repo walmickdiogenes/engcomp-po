80eaafd9b015994f7c5cb47d1e505c1c15a76ec7
Walmick Diogenes <walmick.diogenes@gmail.com>
2019-03-29 23:35:39 -0300

    ct0104

--- quick-sort/ct0104-p1b.py
+++ quick-sort/ct0104-p1b.py
@@ -1,9 +1,10 @@
 # Walmick Diógenes Nogueira de Queirós
 
-#Gerar também séries invertidas de 200000 400000 600000 800000 e 1000000 de números e plotar o gráfico do tempo.
+# Gerar séries invertidas de 200000 400000 600000 800000 e 1000000 de números e plotar o gráfico do tempo.
 
 from random import randint
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import itertools as it
 import timeit
 
@@ -13,32 +14,22 @@ lista3 = sorted(list(range(60)), reverse=True)
 lista4 = sorted(list(range(80)), reverse=True)
 lista5 = sorted(list(range(100)), reverse=True)
 
-geralista = [lista1, lista2, lista3, lista4, lista5]
+geralista = list[lista1, lista2, lista3, lista4, lista5]
 
 def quickSort(listToSort):
-    # low and high lists
     L = []
     R = []
-    # if the list has less than one element, return it
     if len(listToSort) <= 1:
         return listToSort
-    # the pivot is a number in the list middle
     pivot = listToSort[len(listToSort) // 2]
     for i in listToSort:
-        # append the smaller numbers in the left array
         if i < pivot:
             L.append(i)
-        # append the higher numbers in the right array
         if i > pivot:
             R.append(i)
-    # the sorted list is the left + pivot + right numbers
     return quickSort(L) + [pivot] + quickSort(R)
 
-
 mpl.use('Agg')
-import matplotlib.pyplot as plt
-
-
 def desenhaGrafico(x, y, xl="Tamanho", yl="Tempo"):
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
@@ -48,13 +39,11 @@ def desenhaGrafico(x, y, xl="Tamanho", yl="Tempo"):
     plt.xlabel(xl)
     fig.savefig('casos2.png')
 
-
 varrer = []
 
 for i in geralista:
-    #varrido = list(i)
     varrer.append(timeit.timeit("quickSort({})".format(geralista), setup="from __main__ import quickSort", number=1))
 
 print(quickSort(geralista))
 
-desenhaGrafico(tamlista, varrer)
+#desenhaGrafico(geralista, varrer)
--- quick-sort/ct0104-p2.py
+++ quick-sort/ct0104-p2.py
@@ -2,3 +2,46 @@
 
 #Mostrar qual realmente eh o pior caso para o algoritmo de seleção usando uma serie de 6 números
 #(Gerar todas as permutações possíveis e mostrar o pior e o melhor resultados)
+
+from random import randint
+import matplotlib as mpl
+import itertools as it
+import timeit
+
+permutacao = (list(it.permutations(range(6))))
+
+def insertionSort(alist):
+   for i in range(1,len(alist)):
+       current = alist[i]
+       while i>0 and alist[i-1]>current:
+            alist[i] = alist[i-1]
+            i = i-1
+            alist[i] = current
+
+def quickSort(listToSort):
+    L = []
+    R = []
+    if len(listToSort) <= 1:
+        return listToSort
+    pivot = listToSort[len(listToSort) // 2]
+    for i in listToSort:
+        if i < pivot:
+            L.append(i)
+        if i > pivot:
+            R.append(i)
+    return quickSort(L) + [pivot] + quickSort(R)
+
+tempo = []
+
+for i in permutacao:
+    permuta = list(i)
+    tempo.append(timeit.timeit("quickSort({})".format(permuta),setup="from __main__ import quickSort",number=1))
+
+
+melhorTempo = tempo.index(min(tempo))
+piorTempo = tempo.index(max(tempo))
+
+print(min(tempo))
+print(permutacao[melhorTempo])
+print(max(tempo))
+print(permutacao[piorTempo])
